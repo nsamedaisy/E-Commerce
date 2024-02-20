68388b47bcfaee514cf07f902445940d4f978dc8
nsamedaisy <nsamedaisy@gmail.com>
2024-02-20 23:46:50 +0100

    working on the home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ProductList from "./components/ProductList";
+import Home from "./components/home";
 
 const App = () => {
   return (
     <div>
       <h3>Welcome to this app</h3>
+      <Home />
       <ProductList />
     </div>
   );
