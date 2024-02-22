be413f5b6c9b86ef9c8c5f72f7d40a566cdd0f79
nsamedaisy <nsamedaisy@gmail.com>
2024-02-22 23:25:25 +0100

    home page updates

--- src/components/home.js
+++ src/components/home.js
@@ -1,27 +1,42 @@
 import React from "react";
-import Image from "../assets/img.jpg";
+import { Link } from "react-router-dom";
+import Image from "../assets/img1.jpg";
+import { FiSearch, FiShoppingCart } from "react-icons/fi";
 
 const HomePage = () => {
   return (
-    <div className="bg-blue-500">
-      <header>
+    <div>
+      <header className="shadow-xl">
         <div className="top-header">
-          <img src={Image} alt="header" className="h-64" />
+          <img src={Image} alt="header" className="h-10 w-full object-cover" />
           {/* Background image goes here */}
         </div>
-        <div className="bottom-header">
+
+        <div className="flex items-center bg-white rounded-md px-2 py-1 border">
+          <input
+            type="text"
+            placeholder="Search"
+            className="w-40 p-1 focus:outline-none"
+          />
+          <span className="ml-2">
+            <FiSearch className="w-5 h-5" />
+          </span>
+        </div>
+        <span className="ml-4">
+          <FiShoppingCart className="w-5 h-5" />
+        </span>
+
+        <div className="mx-20 py-10">
           <nav>
-            <ul>
+            <ul className="flex justify-between">
               <li>
-                <a href="/body-care" className="text-bold text-6xl">
-                  Body Care
-                </a>
+                <Link to="/body-care">Body Care</Link>
               </li>
               <li>
-                <a href="/home-fragrance">Home Fragrance</a>
+                <Link to="/home-fragrance">Home Fragrance</Link>
               </li>
               <li>
-                <a href="/gifts">Gifts</a>
+                <Link to="/gifts">Gifts</Link>
               </li>
             </ul>
           </nav>
